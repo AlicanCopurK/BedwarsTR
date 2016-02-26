9343962b1296fdd5fe331dd3c027cd5c139465f6
Enes Yıldırım <enes5519@gmail.com>
2016-02-27 01:02:38 +0200

    Main Güncellendi

--- src/BedwarsTR/Bedwars.php
+++ src/BedwarsTR/Bedwars.php
@@ -1,1 +1,15 @@
 
+<?php
+
+namespace BedwarsTR;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\events\Listener;
+
+class Bedwars extends PluginBase implements Listener{
+    public function onEnable(){
+      $this->getLogger()->info("§6[§fBedWarsTR§6]§a Plugin aktifleştirldi")
+    }
+
+}
+?>
