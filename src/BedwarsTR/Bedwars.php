89ef0a351ef3806a8b11932a0eb475ea14f28f8d
AlicanCopurK <vucistou@dodsi.com>
2016-02-27 15:29:19 +0200

    Guncellestirildi

--- src/BedwarsTR/Bedwars.php
+++ src/BedwarsTR/Bedwars.php
@@ -12,7 +12,7 @@ class Bedwars extends PluginBase implements Listener{
     public function onEnable(){
       $this->getLogger()->info("§6[§fBedWarsTR§6]§a Plugin aktifleştirildi")
     }
-    public function onCommand(CommandSender $sender, Command $cmmd, $label, array $args){
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
 
     }
 
